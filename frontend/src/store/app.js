fc9ee74964079ad03c4c32403c0cddde0f9dcdc8
dtjww <97948483+dtjww@users.noreply.github.com>
2023-10-09 21:32:04 +0800

    SPM-52 State Management completed

--- frontend/src/store/app.js
+++ frontend/src/store/app.js
@@ -6,6 +6,8 @@ import axios from "axios"
 const serviceURL = import.meta.env.VITE_SERVICE_URL
 
 export const useAppStore = defineStore('app', {
+  state: () => ({
+    loggedInUser: null,
   // state: () => ({
   // apiURL: serviceURL
   // }),
@@ -42,6 +44,43 @@ export const useAppStore = defineStore('app', {
   //     // }
   //   }
   // }
+}),
+actions: {
+  async login (username) {
+    try {
+      const response = {username};
+    if (response.status === 200) {
+      this.saveResponsetoStore(response);
+      this.saveUsertoLocalStorage();
+    }
+    return response;
+  }
+    catch (error) {
+      console.error(error);
+      throw error;
+    }
+  },
+  saveResponsetoStore(role) {
+    this.loggedInUser = role;
+    this.saveUsertoLocalStorage();
+  },
+  saveUsertoLocalStorage() {
+    const item = window.localStorage.getItem("loggedInUser");
+    console.log(this.loggedInUser);
+    window.localStorage.removeItem("loggedInUser");
+    window.localStorage.setItem("loggedInUser", JSON.stringify(this.loggedInUser)); 
+  },
+  loadUserFromLocalStorage() {
+    const user = localStorage.getItem("loggedInUser");
+    if (user) {
+      this.loggedInUser = JSON.parse(user);
+    }
+  },
+  logout() {
+    this.loggedInUser = null;
+    localStorage.removeItem("loggedInUser");
+  }
+}
 })
 
 
