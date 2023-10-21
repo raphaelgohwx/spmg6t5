5ddb74864c8ea9530e065e4c360e13e678c81d21
Kenneth Lim <kenneth.lim.2021@smu.edu.sg>
2023-10-21 23:27:27 +0800

    SPM-43
    Validations for update role listing done

--- frontend/src/views/UpdateRole.vue
+++ frontend/src/views/UpdateRole.vue
@@ -4,7 +4,7 @@
     </div>
     <div>
       <h2>Update Role Listing</h2>
-      <form @submit.prevent="updateRoleListing">
+      <form @submit.prevent="updateRoleListing" class="role-listing-form">
         <div class="form-group">
           <label for="listingID">Listing ID:</label>
           <select id="listingID" v-model="listingID" required>
@@ -12,26 +12,50 @@
               <option v-for="listing in listingIDList" :key="listing" :value="listing">{{ listing }}</option>
             </select>
         </div>
+
         <div class="form-group">
           <label for="roleName">Role Name:</label>
-          <input type="text" id="roleName" v-model="roleName" required />
+            <select id="roleName" v-model="roleName" required>
+              <option :value="null" disabled selected>Select Role</option>
+              <option v-for="role in roles" :key="role" :value="role">{{ role }}</option>
+            </select>
+            <span v-if="roleName.length == 0" class="error">
+              Note: Please select a role name.
+            </span>
         </div>
+
         <div class="form-group">
           <label for="roleDescription">Description:</label>
-          <input type="text" id="roleDescription" v-model="roleDescription" required />
+          <textarea id="roleDescription" v-model="roleDescription" required 
+          @input="resizeTextarea" />
+          <span v-if="roleDescription.length === 0" class="error">
+              Note: Description must be between 1 and 100 characters.
+            </span>
+            <span v-if="roleDescription.length > 100" class="error">
+              Note: Description cannot be more than 100 characters long.
+            </span>
         </div>
+
         <div class="form-group">
           <label for="department">Department:</label>
-          <input type="text" id="department" v-model="department" required />
+            <select id="department" v-model="department" required>
+              <option :value="null" disabled selected>Select Dept</option>
+              <option v-for="dept in depts" :key="dept" :value="dept">{{ dept }}</option>
+            </select>
+            <span v-if="department.length == 0" class="error">
+              Note: Please select a department.
+            </span>
         </div>
+
         <div class="form-group">
           <label for="closingDate">Closing Date:</label>
           <input type="date" id="closingDate" v-model="closingDate" required />
+          <span v-if="!this.closingDateValidation()" class="error">
+              Note: Please select a closing date in the future.
+            </span>
         </div>
-        <button type="submit" class="btn btn-primary">Update Role Listing</button>
+        <button type="submit" class="btn btn-primary" :disabled="formValidation()">Update Role Listing</button>
       </form>
-      <!-- Testing -->
-      <v-btn @click='getAllUpdatedListings()'>Test updated listings</v-btn>
     </div>
   </template>
   
@@ -52,7 +76,6 @@
         roleDescription: "",   
         department: "",      
         closingDate: "",
-        depts: [],
         roles: ["Account Manager",
                 "Admin Team",
                 "Call Centre",
@@ -68,14 +91,40 @@
                 "Sales Manager",
                 "Senior Engineer",
                 "Support Team"],
+        depts: ["IT",
+                "Sales",
+                "Consultancy",
+                "Finance",
+                "HR and Admin",
+                "Engineering Operation Division"],
       };
     },
     mounted() {
       // Fetch existing role listing data from the backend
       this.fetchRoleListingData();
-    
+
     },
     methods: {
+      // For visuals only
+      resizeTextarea() {
+        const textarea = document.getElementById('roleDescription');
+        textarea.style.height = 'auto'; // Reset the height to auto to calculate the actual content height
+        textarea.style.height = textarea.scrollHeight + 'px'; // Set the height to match the content height
+      },
+
+      // checking if selected closingDate is in the future
+    closingDateValidation(){
+        const dateToday = new Date();
+        const dateSelected = new Date(this.closingDate);
+        return dateToday <= dateSelected;
+      },
+
+      // checking if any of the fields have error, to prevent form submission, hasErrors is true if any of the fields have error
+      formValidation() {
+        const hasErrors = this.roleName.length === 0 || this.roleDescription.length > 100 || this.roleDescription.length === 0 || this.department.length === 0 || !this.closingDateValidation();
+        return hasErrors;
+      },
+
       async fetchRoleListingData() {
         //API request to fetch the existing role listing data
         try {
@@ -92,6 +141,7 @@
           console.error("Error fetching role listing data:", error);
         }
       },
+
       async updateRoleListing() {
         // Create an object with updated role listing data
         const updatedRoleListing = {
@@ -132,8 +182,17 @@
 <style scoped>
   h2{
   margin-top: 10px;
-  margin-bottom: 20px;
-  margin-left: 10px;
+  margin-bottom: 15px;
+  text-align:center;
+}
+
+.role-listing-form {
+  max-width: 800px; 
+  margin: 0 auto; /* Center the form horizontally */
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+
 }
 
 .form-group {
@@ -148,7 +207,7 @@ label {
 }
 
 input[type="text"] {
-  width: 30%;
+  width: 40%;
   padding: 10px;
   border: 1px solid #ccc;
   border-radius: 4px;
@@ -159,6 +218,23 @@ select {
   border: 1px solid #ccc;
   border-radius: 4px;
 }
+
+input[type="date"]{
+  width: 40%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+textarea {
+  width: 40%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  height:auto; 
+  resize: none; /* Allow vertical resizing */
+  overflow-y: hidden; /* Hide vertical scrollbar */
+}
+
 .btn {
   background-color: #007bff;
   color: #fff;
@@ -166,6 +242,7 @@ select {
   padding: 10px 20px;
   border-radius: 4px;
   cursor: pointer;
+  margin-left: 10px;
 }
 
 .btn-primary {
@@ -175,6 +252,10 @@ select {
 .btn-primary:hover {
   background-color: #0056b3; 
   /* darker when you hover over it */
+}
+
+.error{
+  color: #0056b3; 
 }
 </style>
   
